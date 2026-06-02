231d5478e9a670e370060b1d9bc77b3903cc4d83
insa-ship-it <leemichaelinsa@gmail.com>
2026-06-02 14:58:00 -0500

    Fresh EPG Update: Tue Jun  2 20:00:45 UTC 2026

--- update_epg.py
+++ update_epg.py
@@ -45,7 +45,8 @@ URLS = [
     'https://github.com/matthuisman/i.mjh.nz/raw/master/Roku/all.xml',
     'https://epg.pw/api/epg.xml?lang=en&timezone=VVMvRWFzdGVybg%3D%3D&date=20260405&channel_id=464981',
     'https://github.com/insa-ship-it/xumo-playlist-generator/raw/refs/heads/main/playlists/xumo_epg.xml.gz',
-    'https://raw.githubusercontent.com/matthuisman/i.mjh.nz/refs/heads/master/PlutoTV/all.xml'
+    'https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/PlutoTV/us.xml.gz', 
+    'https://github.com/matthuisman/i.mjh.nz/raw/refs/heads/master/PlutoTV/gb.xml.gz' 
 ]
 
 def get_tvg_ids_from_remote_m3u():
